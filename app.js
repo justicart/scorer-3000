0464f9b5c901200e9cc104d3cebf2ea517819c3c
Justin Carter <justin@vidpresso.com>
2017-03-05 01:22:42 -0700

    db switching

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // mongoose.connect( 'mongodb://localhost/scorer' );
-mongoose.connect( process.env.MONGODB_URI );
+const db = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/scorer';
+mongoose.connect( db );
 
-var index = require('./routes/index');
-var players = require('./routes/players');
-var games = require('./routes/games');
-var app = express();
+const index = require('./routes/index');
+const players = require('./routes/players');
+const games = require('./routes/games');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +34,7 @@ app.use('/games', games);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
