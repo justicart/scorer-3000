c896527bdf16bc3c3aefcf2028bc4099c6bb9de2
Justin Carter <justin@vidpresso.com>
2017-03-06 22:26:03 -0700

    Refresh again

--- models/player.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Player = new Schema({
-  name: { type: String },
-  image: { type: String }
-});
-
-module.exports = mongoose.model( 'Player', Player );
