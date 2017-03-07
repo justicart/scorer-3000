51d1cce105694bede5ed2666d1cf5ac2c161520d
Justin Carter <justin@vidpresso.com>
2017-03-07 00:37:08 -0700

    Main page

--- client/components/CreateGame.js
+++ client/components/CreateGame.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 class CreateGame extends React.Component {
+  state = { playerIds: [], holes: 18 };
+
+  togglePlayerIdToGame = (id) => {
+    if (this.state.playerIds.indexOf(id) > -1) {
+      this.setState({ playerIds: this.state.playerIds.filter( p => p !== id ) })
+    } else {
+      this.setState({ playerIds: [...this.state.playerIds, id] })
+    }
+  }
+
   render() {
+    const playerList = this.props.players.map( player => {
+      const active = this.state.playerIds.indexOf(player._id) > -1;
+      return (
+        <a
+          key={player._id}
+          className={`collection-item avatar ${active ? 'active' : ''}`}
+          onClick={() => this.togglePlayerIdToGame(player._id)}
+        >
+          <img src={player.image} alt="" className="circle" />
+          <span className="title">{ player.name }</span>
+          <p>Stats here</p>
+          {active && <span className="secondary-content">
+            <i className="material-icons">done</i>
+          </span>}
+        </a>
+      )
+    });
+
     return (
-      <div>Game</div>
+      <div>
+        <div className="collection col s12 m6">
+           { playerList.length ?
+               playerList :
+               <li className="collection-item">Add players in <Link to={'/settings'}>settings</Link></li>
+           }
+        </div>
+      </div>
     );
   }
 };
 
-export default CreateGame;
+const mapStateToProps = (state) => {
+ return { players: state.players }
+}
+
+export default connect(mapStateToProps)(CreateGame);
